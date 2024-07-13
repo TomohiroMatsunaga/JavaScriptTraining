28d44da0b207a588a5019936f5cc6f561b890acf
TomohiroMatsunaga <Tomohiro.Matsunaga@jp.ricoh.com>
2024-07-13 18:15:10 +0900

    コミットミスを修正

--- ch14/ex05/index.js
+++
@@ -1,6 +0,0 @@
-export function typeNameTemplate(strings, ...values) { //文字列部分と補間部分を受け取る
-    const value = values[0];
-    const typeName = value !== undefined ? typeof value : 'undefined'; //補間部分の方をチェック
-  
-    return typeName;
-  }
