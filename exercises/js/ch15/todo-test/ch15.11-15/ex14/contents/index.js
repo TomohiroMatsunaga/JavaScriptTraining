317161624327e8b20fc627d195f7280e49254c63
TomohiroMatsunaga <Tomohiro.Matsunaga@jp.ricoh.com>
2024-09-12 00:32:43 +0900

    ex14までの回答

--- exercises/js/ch15/todo-test/ch15.11-15/ex14/contents/index.js
+++ exercises/js/ch15/todo-test/ch15.11-15/ex14/contents/index.js
@@ -13,4 +13,30 @@ async function getMessageFromServer() {
   messageContainer.appendChild(messageElement);
 
   // TODO: ここにサーバーとのやり取り等を実装しなさい
+  //---start---
+  // ボタンを無効化
+  button.disabled = true;
+
+  // サーバーとの通信を開始
+  const eventSource = new EventSource("http://localhost:3000/message");
+
+  // サーバーからメッセージが届いたら表示
+  eventSource.onmessage = function (event) {
+    try {
+      const data = JSON.parse(event.data);  // メッセージをJSON形式でパース 例 {"value": "こんにちわ\n", "done": false}
+      const messageElement = document.createElement("div"); //メッセージを表示するコンテナを生成
+      messageElement.className = "message"; //CSS(message)を適応
+      messageElement.textContent = data.value;  // JSONのvalueを表示
+      messageContainer.appendChild(messageElement); // メッセージを表示するコンテナをmessageContainerに追加。
+
+      // doneがtrueなら接続を閉じる
+      if (data.done) {
+        eventSource.close();
+        button.disabled = false;  // ボタンを再び有効化
+      }
+    } catch (error) {
+      console.error("error:", error);
+    }
+  };
+  //---end---
 }
