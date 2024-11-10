72e1eb3380209872a13325e09574cd7ae7d30ac4
Tomohiro Matsunaga <Tomohiro.Matsunaga@jp.ricoh.com>
2024-11-10 19:24:55 +0900

    ex06

--- exercises/js/ch17/webpack.config.js
+++ exercises/js/ch17/webpack.config.js
@@ -4,10 +4,11 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url)); // webpack.config.jsがあるディレクトリを取得
 
 export default {
-  entry: './ex05/index.js', // エントリーポイントのファイルを指定
+  entry: './ex06/index.js', // エントリーポイントのファイルを指定
   output: {
     filename: 'bundle.js', // 出力ファイル名
-    path: path.resolve(__dirname, 'ex05', 'webpack'), // 出力先ディレクトリ
+    path: path.resolve(__dirname, 'ex06', 'webpack'), // 出力先ディレクトリ
   },
   mode: 'production', // デバッグがしやすくなる情報を含むbundleを生成する場合はmode: 'development'
+  devtool: 'source-map', // ソースマップを生成する設定を追加
 };
